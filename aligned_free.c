47118bd46850dcee30e5a1e1f1f6fd757528f0fb
Nick Strupat <nick.strupat@intelex.com>
2015-11-04 11:23:38 -0500

    introduce a fallback memory alignment algorithm for all previously unsupported platforms

--- aligned_free.c
+++ aligned_free.c
@@ -9,11 +9,16 @@ void aligned_free(void * pointer) {
 #elif defined(_WIN32)
 
 #include <crtdbg.h>
-
 void aligned_free(void * pointer) {
 	_aligned_free_dbg(pointer); // This is reduced to a call to `_aligned_free` when _DEBUG is not defined
 }
 
 #else
-#error Unrecognized platform
+
+// https://sites.google.com/site/ruslancray/lab/bookshelf/interview/ci/low-level/write-an-aligned-malloc-free-function
+#include <stdlib.h>
+void aligned_free(void * pointer) {
+    free(((void * *)pointer)[-1]);
+}
+
 #endif
--- aligned_malloc.c
+++ aligned_malloc.c
@@ -3,7 +3,6 @@
 #if defined(__APPLE__) || defined(__linux__)
 
 #include <stdlib.h>
-
 void * aligned_malloc(size_t size, size_t alignment) {
 	void * pointer;
 	posix_memalign(&pointer, alignment, size);
@@ -13,11 +12,23 @@ void * aligned_malloc(size_t size, size_t alignment) {
 #elif defined(_WIN32)
 
 #include <crtdbg.h>
-
 void * aligned_malloc(size_t size, size_t alignment) {
 	return _aligned_malloc_dbg(size, alignment); // This is reduced to a call to `_aligned_malloc` when _DEBUG is not defined
 }
 
 #else
-#error Unrecognized platform
+
+// https://sites.google.com/site/ruslancray/lab/bookshelf/interview/ci/low-level/write-an-aligned-malloc-free-function
+#include <stdlib.h>
+void * aligned_malloc(size_t size, size_t alignment) {
+	void * p1; // original block
+	void ** p2; // aligned block
+	int offset = alignment - 1 + sizeof(void *);
+	if ((p1 = (void *)malloc(size + offset)) == NULL)
+		return NULL;
+	p2 = (void **)(((size_t)(p1) + offset) & ~(alignment - 1));
+	p2[-1] = p1;
+	return p2;
+}
+
 #endif
