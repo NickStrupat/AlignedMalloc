784c00e1c7798fad0cf1347fcc87efa4f5de26e5
Gabriel Ferreira <gabrielcarvfer@gmail.com>
2019-10-13 23:35:51 +0200

    C++ guards and inclusion of standard library that includes free

--- aligned_free.h
+++ aligned_free.h
@@ -1,6 +1,19 @@
 #ifndef ALIGNED_FREE_H_INCLUDED
 #define ALIGNED_FREE_H_INCLUDED
 
+
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 void aligned_free(void * pointer);
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
